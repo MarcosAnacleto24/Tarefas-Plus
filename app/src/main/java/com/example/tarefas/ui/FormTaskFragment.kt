0c4e54dcd5888f6b28e499b731dde7723eaf0272
Marcos Anacleto <marcosanacleto66@gmail.com>
2025-08-21 10:24:53 -0300

    Criar layout de adicionar tareas Adicionar botão de add Configurar navegação

--- app/src/main/java/com/example/tarefas/ui/FormTaskFragment.kt
+++ app/src/main/java/com/example/tarefas/ui/FormTaskFragment.kt
@@ -5,37 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.example.tarefas.R
-import com.example.tarefas.databinding.FragmentTodoBinding
+import com.example.tarefas.databinding.FragmentFormTaskBinding
 
 
-class TodoFragment : Fragment() {
+class FormTaskFragment : Fragment() {
 
-    private var _binding: FragmentTodoBinding? = null
-
+    private var _binding: FragmentFormTaskBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentTodoBinding.inflate(inflater, container, false)
+    ): View? {
+       _binding = FragmentFormTaskBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        initListeners()
     }
 
-    private fun initListeners() {
-        binding.fabAdd.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
-        }
-    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
