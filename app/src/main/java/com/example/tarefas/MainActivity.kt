7a510213e5e5d634fc2b0a4cca3e0e61c68b6e86
Marcos Anacleto <marcosanacleto66@gmail.com>
2025-08-27 15:18:36 -0300

    Corrigir bug versâo de android Criar layout item_task

--- app/src/main/java/com/example/tarefas/MainActivity.kt
+++ app/src/main/java/com/example/tarefas/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tarefas.databinding.ActivityMainBinding
 
