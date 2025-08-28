9fba30cfcce2d134599925017724acfbf1642794
Marcos Anacleto <marcosanacleto66@gmail.com>
2025-08-28 13:03:38 -0300

    Criar adapter RecyclerView Criar data class Task

---
+++ app/src/main/java/com/example/tarefas/data/model/Task.kt
@@ -0,0 +1,11 @@
+package com.example.tarefas.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Task(
+    val id: String,
+    val description: String
+
+) : Parcelable
