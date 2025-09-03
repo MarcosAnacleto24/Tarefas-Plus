f2211d6e94aad79377df081f06115adfec9efa95
Marcos Anacleto <marcosanacleto66@gmail.com>
2025-09-03 14:58:05 -0300

    Deslogar usuário

--- app/src/main/java/com/example/tarefas/ui/HomeFragment.kt
+++ app/src/main/java/com/example/tarefas/ui/HomeFragment.kt
@@ -6,16 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.tarefas.R
 import com.example.tarefas.databinding.FragmentHomeBinding
 import com.example.tarefas.ui.adapter.ViewPagerAdapter
+import com.example.tarefas.util.showBottomSheet
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreateView(
@@ -28,7 +34,26 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+
+        initListeners()
         initTabs()
+
+    }
+
+    private fun initListeners() {
+        binding.btnLogout.setOnClickListener {
+           showBottomSheet(
+               titleDialog = R.string.title_dialog,
+               titleButton = R.string.title_button,
+               message = getString(R.string.title_message),
+               onClick = {
+                   auth.signOut()
+                   findNavController().navigate(R.id.action_homeFragment_to_authentication)
+               }
+           )
+        }
     }
 
     private fun initTabs() {
