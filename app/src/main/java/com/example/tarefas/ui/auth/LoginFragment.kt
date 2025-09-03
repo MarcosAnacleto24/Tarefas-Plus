3350b88e667505f7622e42616c068086247e32da
Marcos Anacleto <marcosanacleto66@gmail.com>
2025-09-03 15:08:17 -0300

    Logar usuário

--- app/src/main/java/com/example/tarefas/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/tarefas/ui/auth/LoginFragment.kt
@@ -6,17 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.example.tarefas.R
 import com.example.tarefas.databinding.FragmentLoginBinding
 import com.example.tarefas.util.showBottomSheet
-
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreateView(
@@ -30,6 +34,7 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = Firebase.auth
         initListeners()
 
     }
@@ -54,13 +59,26 @@ class LoginFragment : Fragment() {
         
         if (email.isNotEmpty()) {
             if (password.isNotEmpty()) {
-                findNavController().navigate(R.id.action_global_homeFragment)
+                binding.progressBar.isVisible = true
+                loginUser(email, password)
             } else {
                 showBottomSheet(message = getString(R.string.password_empty) )
             }
         } else {
             showBottomSheet(message = getString(R.string.email_empty))
         }
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_global_homeFragment)
+                } else {
+                   binding.progressBar.isVisible = false
+                    Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     override fun onDestroyView() {
