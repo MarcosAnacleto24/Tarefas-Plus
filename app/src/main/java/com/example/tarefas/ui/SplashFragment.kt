c8ca8aecb14a58551e90ed7a3a97eafa3fc1607c
Marcos Anacleto <marcosanacleto66@gmail.com>
2025-09-03 11:24:12 -0300

    Verificar se usuário já está autenticado

--- app/src/main/java/com/example/tarefas/ui/SplashFragment.kt
+++ app/src/main/java/com/example/tarefas/ui/SplashFragment.kt
@@ -10,11 +10,15 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.tarefas.R
 import com.example.tarefas.databinding.FragmentSplashBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class SplashFragment : Fragment() {
 
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreateView(
@@ -28,11 +32,20 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = Firebase.auth
+
         Handler(Looper.getMainLooper()).postDelayed(this::checkAuth,2000)
     }
 
     private fun checkAuth() {
-        findNavController().navigate(R.id.action_splashFragment_to_authentication)
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        } else {
+            findNavController().navigate(R.id.action_splashFragment_to_authentication)
+        }
+
     }
 
     override fun onDestroyView() {
